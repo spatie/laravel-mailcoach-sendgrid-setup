b297ac3a35bdc05388b9aa1db8517034a0b04848
riasvdv <rias@spatie.be>
2022-05-30 15:02:59 +0000

    wip

--- tests/SendgridTest.php
+++ tests/SendgridTest.php
@@ -6,23 +6,23 @@ use Spatie\MailcoachSendgridSetup\Tests\TestCase;
 
 uses(TestCase::class);
 
-beforeEach(function() {
+beforeEach(function () {
     $this->sendGrid = new Sendgrid($this->key);
 });
 
-it('can determine an api key is valid', function() {
+it('can determine an api key is valid', function () {
     $result = $this->sendGrid->isValidApiKey();
 
     expect($result)->toBeTrue();
 });
 
-it('can determine an api key is invalid', function() {
+it('can determine an api key is invalid', function () {
     $result = (new Sendgrid('invalid-key'))->isValidApiKey();
 
     expect($result)->toBeFalse();
 });
 
-it('can update the webhook settings', function() {
+it('can update the webhook settings', function () {
     $url = "https://test-url.com/first";
     $this->sendGrid->setupWebhook($url, [EventType::Open]);
 
@@ -31,6 +31,9 @@ it('can update the webhook settings', function() {
         ->and($webhookSettings['open'])->toBeTrue()
         ->and($webhookSettings['click'])->toBeFalse();
 
+    expect($this->sendGrid->openTrackingEnabled())->toBeTrue();
+    expect($this->sendGrid->clickTrackingEnabled())->toBeFalse();
+
     $url = "https://test-url.com/second";
     $this->sendGrid->setupWebhook($url, [EventType::Click]);
 
@@ -38,12 +41,31 @@ it('can update the webhook settings', function() {
     expect($webhookSettings['url'])->toBe($url)
         ->and($webhookSettings['open'])->toBeFalse()
         ->and($webhookSettings['click'])->toBeTrue();
+
+    expect($this->sendGrid->openTrackingEnabled())->toBeFalse();
+    expect($this->sendGrid->clickTrackingEnabled())->toBeTrue();
 });
 
-it('can get webhook settings', function() {
-   $webhookSettings = $this->sendGrid->getWebhook();
+it('can enable and disable open tracking on the account', function() {
+   $this->sendGrid->enableOpenTracking();
+   expect($this->sendGrid->openTrackingEnabled())->toBeTrue();
 
-   expect($webhookSettings)->toHaveKeys([
+    $this->sendGrid->enableOpenTracking(false);
+    expect($this->sendGrid->openTrackingEnabled())->toBeFalse();
+});
+
+it('can enable and disable click tracking on the account', function() {
+    $this->sendGrid->enableClickTracking();
+    expect($this->sendGrid->clickTrackingEnabled())->toBeTrue();
+
+    $this->sendGrid->enableClickTracking(false);
+    expect($this->sendGrid->clickTrackingEnabled())->toBeFalse();
+});
+
+it('can get webhook settings', function () {
+    $webhookSettings = $this->sendGrid->getWebhook();
+
+    expect($webhookSettings)->toHaveKeys([
        'url',
        'open',
        'click',
@@ -52,7 +74,3 @@ it('can get webhook settings', function() {
        'enabled',
    ]);
 });
-
-
-
-
